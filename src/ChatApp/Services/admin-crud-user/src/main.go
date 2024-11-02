a40243bafc2e279e4944b4dca55e479505f22069
Tran Tho <trandinhttho@gmail.com>
2024-11-02 13:01:57 +0700

    add environment variables

--- src/ChatApp/Services/admin-crud-user/src/main.go
+++ src/ChatApp/Services/admin-crud-user/src/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/Apolos666/ChatApp/src/api"
 	db "github.com/Apolos666/ChatApp/src/db/sqlc"
@@ -13,7 +15,7 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	connPool, err := pgxpool.New(ctx, "postgresql://postgres:phamtantudn3@postgres:5432/chat_app?sslmode=disable")
+	connPool, err := connectDB(ctx)
 	if err != nil {
 		log.Fatalf("cannot connect to db: %v", err)
 	}
@@ -25,4 +27,18 @@ func main() {
 	server := api.NewServer(repo)
 
 	server.Start(":8085")
+}
+
+func connectDB(ctx context.Context) (*pgxpool.Pool, error) {
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPassword := os.Getenv("DATABASE_PASSWORD")
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+	dbName := os.Getenv("DATABASE_NAME")
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPassword, dbHost, dbPort, dbName)
+	connPool, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		return nil, err
+	}
+	return connPool, nil
 }
