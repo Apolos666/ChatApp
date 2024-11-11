17b929d51e6f13e2e43b2c400fe210efc26b478e
tdn283 <nhatflame@gmail.com>
2024-11-11 17:18:49 +0700

    fix: modified admin api url to match backend side

--- src/ChatApp/Webs/chatapp/src/services/user.service.api/_adminReq.ts
+++ src/ChatApp/Webs/chatapp/src/services/user.service.api/_adminReq.ts
@@ -3,7 +3,7 @@ import { PersistedStateKey } from '@/data/persisted-keys'
 import { getLocalStorageItem } from '@/utils/local-storage'
 
 const adminInstance = axios.create({
-  baseURL: 'http://localhost:8085/api',
+  baseURL: 'http://127.0.0.1:8085/api',
 })
 
 adminInstance.interceptors.request.use(
